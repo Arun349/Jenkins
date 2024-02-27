886e4193563fc6abc0a6934b5faa78dd996e2d0a
Arun <arunsrinivasan333@gmail.com>
2024-02-27 11:39:29 +0530

    Docker COmmit

--- JenkinsDemo/obj/Debug/net8.0/JenkinsDemo.AssemblyInfo.cs
+++ JenkinsDemo/obj/Debug/net8.0/JenkinsDemo.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d64cb38a-44c3-44a7-b1f6-25f5d85f5411")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("JenkinsDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
